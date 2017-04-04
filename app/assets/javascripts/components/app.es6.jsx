050b3594b07ea90545f7fef703ba0ac68697a517
William Collins <wcurtiscollins@gmail.com>
2017-04-04 00:08:36 -0400

    setup webpack

--- app/assets/javascripts/components/app.es6.jsx
+++
@@ -1,9 +0,0 @@
-class App extends React.Component {
-  render () {
-    return (
-      <div>
-        <div>My App: {this.props.app}</div>
-      </div>
-    );
-  }
-}
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  // the base path which will be used to resolve entry points
+  context: __dirname,
+  // the main entry point for our application's frontend JS
+  entry: './app/frontend/javascripts/entry.js',
+
+  output:  {
+    // this is our app/assets/javascripts directory, which is part of the Sprockets pipeline
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    // the filename of the compiled bundle, e.g. app/assets/javascripts/bundle.js
+    filename: 'application.js',
+    // if the webpack code-splitting feature is enabled, this is the path it'll use to download bundles
+    publicPath: '/assets',
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+
+};
+
