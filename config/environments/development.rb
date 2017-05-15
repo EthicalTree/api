f9de7a893b5222f71ef4167ec792a18486f73d3e
William Collins <wcurtiscollins@gmail.com>
2017-05-14 23:47:18 -0400

    add hot reloading

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,6 +27,13 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  # Serve assets from webpack-dev-server
+  config.action_controller.asset_host = Proc.new { |source|
+    if source =~ /application\.js$/i
+    "http://ethicaltree:8080/assets"
+    end
+  }
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
--- webpack.config.js
+++ webpack.config.js
@@ -4,24 +4,57 @@ var webpack = require('webpack');
 module.exports = {
   // the base path which will be used to resolve entry points
   context: __dirname,
+
   // the main entry point for our application's frontend JS
-  entry: './app/frontend/javascripts/entry.js',
+  entry: {
+    client: 'webpack-dev-server/client?http://localhost:8080',
+    // bundle the client for webpack-dev-server
+    // and connect to the provided endpoint
+
+    hot: 'webpack/hot/only-dev-server',
+    // bundle the client for hot reloading
+    // only- means to only hot reload for successful updates
+
+    application: './app/frontend/javascripts/entry.js'
+  },
+
   devtool: 'cheap-module-source-map',
 
   plugins: [
     new webpack.LoaderOptionsPlugin({
       debug: true
-    })
+    }),
+
+    new webpack.HotModuleReplacementPlugin(),
+    // enable HMR globally
+
+    new webpack.NamedModulesPlugin(),
+    // prints more readable module names in the browser console on HMR updates
+
+    new webpack.NoEmitOnErrorsPlugin(),
+    // do not emit compiled assets that include errors
   ],
+
+  devServer: {
+    host: 'localhost',
+    historyApiFallback: true,
+    // respond to 404s with index.html
+    headers: { 'Access-Control-Allow-Origin': '*'  },
+
+    contentBase: path.join(__dirname, 'app', 'assets', 'javascripts'),
+
+    hot: true,
+    // enable HMR on the server
+  },
 
   output:  {
     // this is our app/assets/javascripts directory, which is part of the Sprockets pipeline
     path: path.join(__dirname, 'app', 'assets', 'javascripts'),
     // the filename of the compiled bundle, e.g. app/assets/javascripts/bundle.js
-    filename: 'application.js',
+    filename: '[name].js',
     sourceMapFilename: "[file].map",
     // if the webpack code-splitting feature is enabled, this is the path it'll use to download bundles
-    publicPath: '/assets',
+    publicPath: 'http://localhost:8080/assets/javascripts',
   },
 
   module: {
