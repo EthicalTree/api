eeb2c5ce8c6e93b7304bffbb5a677453c215c90c
William Collins <wcurtiscollins@gmail.com>
2017-05-29 01:31:36 -0400

    turn digests off for now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,11 +26,7 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  config.action_controller.asset_host = Proc.new { |source|
-    if source =~ /application\.js$/i
-    "/assets"
-    end
-  }
+  config.assets.digest = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
