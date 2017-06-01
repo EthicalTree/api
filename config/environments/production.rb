b0fddedec41dfd849fbf0b1fc336f83ac5d828e9
William Collins <wcurtiscollins@gmail.com>
2017-05-31 23:57:40 -0400

    add proper origin to production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,7 +105,7 @@ Rails.application.configure do
 # CORS settings
   config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins '*.ethicaltree.com'
+      origins 'http://beta.ethicaltree.com'
       resource '*', :headers => :any, :methods => [:get, :post, :patch, :put, :delete, :options]
     end
   end
