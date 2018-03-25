e552342d2d90e108a109abc5fc55fcbe8ef0b9c9
William Collins <wcurtiscollins@gmail.com>
2018-03-25 13:51:23 -0400

    dockerizing

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -14,24 +14,24 @@ worker_processes 1
 
 # Help ensure your application will always spawn in the symlinked
 # "current" directory that Capistrano sets up.
-working_directory '/home/applepicke/apps/ethicaltree-api/current' # available in 0.94.0+
+working_directory '/app' # available in 0.94.0+
 
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
-listen "/tmp/unicorn.sock"
+listen "/app/tmp/unicorn.sock"
 # listen 8080, :tcp_nopush => true
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
 timeout 60
 
 # feel free to point this anywhere accessible on the filesystem
-pid "/home/applepicke/apps/ethicaltree-api/current/tmp/pids/unicorn.pid"
+pid "/app/tmp/pids/unicorn.pid"
 
 # By default, the Unicorn logger will write to stderr.
 # Additionally, ome applications/frameworks log to stderr or stdout,
 # so prevent them from going to /dev/null when daemonized here:
-stderr_path "/home/applepicke/logs/ethicaltree/unicorn.stderr.log"
-stdout_path "/home/applepicke/logs/ethicaltree/unicorn.stdout.log"
+stderr_path "/app/log/unicorn.stderr.log"
+stdout_path "/app/log/unicorn.stdout.log"
 
 # combine REE with "preload_app true" for memory savings
 # http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
@@ -51,7 +51,7 @@ before_fork do |server, worker|
   # as there's no need for the master process to hold a connection
   defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
 
-  old_pid = "/home/applepicke/pids/unicorn.pid.oldbin"
+  old_pid = "/app/tmp/pids/unicorn.pid.oldbin"
   if File.exists?(old_pid) && server.pid != old_pid
     begin
       Process.kill("QUIT", File.read(old_pid).to_i)
@@ -111,5 +111,5 @@ end
 
 # Explicitly set the Gemfile location so a restarting Unicorn can find new gems
 before_exec do |server|
-  ENV['BUNDLE_GEMFILE'] = File.join('/home/applepicke/apps/ethicaltree-api/current', 'Gemfile')
+  ENV['BUNDLE_GEMFILE'] = File.join('/app', 'Gemfile')
 end
