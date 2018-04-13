fc8eaa37d4d7f37ee656069bbee7f3172951ad00
William Collins <wcurtiscollins@gmail.com>
2018-04-12 23:23:30 -0400

    escape html to prevent XSS and broken pages on load

--- app/models/opengraph.rb
+++ app/models/opengraph.rb
@@ -21,27 +21,35 @@ class Opengraph
   private
 
   def self.get_meta_for_listing listing
-    if listing.present?
-      "
-        <meta property=\"og:url\" content=\"#{@url}\">
-        <meta property=\"og:title\" content=\"#{listing.title}\">
-        <meta property=\"og:description\" content=\"#{listing.bio}\">
-        <meta property=\"og:image\" content=\"#{listing.cover_image}\">
-        <meta name=\"twitter:card\" content=\"summary_large_image\">
-        <meta name=\"fb:app_id\" content=\"#{Rails.application.secrets[:fb_app_id]}\">
-      "
-    else
-      ""
+    self.generate_meta listing do |item|
+      {
+        name: listing.title,
+        description: listing.bio,
+        image: listing.cover_image
+      }
     end
   end
 
   def self.get_meta_for_curated_list curated_list
-    if curated_list.present?
+    self.generate_meta curated_list do |item|
+      {
+        name: curated_list.name,
+        description: curated_list.description,
+        image: curated_list._listings.first.cover_image
+      }
+    end
+  end
+
+  def self.generate_meta item
+    if item.present?
+      options = yield(item)
+      options.each {|k, v| options[k] = CGI::escapeHTML(v)}
+
       "
         <meta property=\"og:url\" content=\"#{@url}\">
-        <meta property=\"og:title\" content=\"#{curated_list.name}\">
-        <meta property=\"og:description\" content=\"#{curated_list.description}\">
-        <meta property=\"og:image\" content=\"#{curated_list._listings.first.cover_image}\">
+        <meta property=\"og:title\" content=\"#{options[:name]}\">
+        <meta property=\"og:description\" content=\"#{options[:description]}\">
+        <meta property=\"og:image\" content=\"#{options[:image]}\">
         <meta name=\"twitter:card\" content=\"summary_large_image\">
         <meta name=\"fb:app_id\" content=\"#{Rails.application.secrets[:fb_app_id]}\">
       "
