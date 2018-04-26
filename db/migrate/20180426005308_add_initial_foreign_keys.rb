381f0e8b7a5eb485d52af22d742c6df0fb6456a6
William Collins <wcurtiscollins@gmail.com>
2018-04-25 22:12:37 -0400

    have migrate check to see if contraint exists first

--- db/migrate/20180426005308_add_initial_foreign_keys.rb
+++ db/migrate/20180426005308_add_initial_foreign_keys.rb
@@ -1,27 +1,33 @@
+def add_foreign_key_safe table, column, relation
+  if !foreign_key_exists?(table, column: column)
+    add_foreign_key table, relation
+  end
+end
+
 class AddInitialForeignKeys < ActiveRecord::Migration[5.2]
   def up
-    add_foreign_key :operating_hours, :listings
+    add_foreign_key_safe :operating_hours, :listing_id, :listings
 
     change_column :curated_lists, :tag_id, :bigint
-    add_foreign_key :curated_lists, :tags
+    add_foreign_key_safe :curated_lists, :tag_id, :tags
 
-    add_foreign_key :listing_ethicalities, :ethicalities
-    add_foreign_key :listing_ethicalities, :listings
+    add_foreign_key_safe :listing_ethicalities, :ethicality_id, :ethicalities
+    add_foreign_key_safe :listing_ethicalities, :listing_id, :listings
 
-    add_foreign_key :locations, :listings
+    add_foreign_key_safe :locations, :listing_id, :listings
 
-    add_foreign_key :listing_images, :images
-    add_foreign_key :listing_images, :listings
+    add_foreign_key_safe :listing_images, :image_id, :images
+    add_foreign_key_safe :listing_images, :listing_id, :listings
 
-    add_foreign_key :menus, :listings
+    add_foreign_key_safe :menus, :listing_id, :listings
     change_column :menu_images, :menu_id, :bigint
 
-    add_foreign_key :menu_images, :menus
-    add_foreign_key :menu_images, :images
+    add_foreign_key_safe :menu_images, :menu_id, :menus
+    add_foreign_key_safe :menu_images, :image_id, :images
 
     change_column :listing_tags, :tag_id, :bigint
-    add_foreign_key :listing_tags, :tags
-    add_foreign_key :listing_tags, :listings
+    add_foreign_key_safe :listing_tags, :tag_id, :tags
+    add_foreign_key_safe :listing_tags, :listing_id, :listings
   end
 
   def down
