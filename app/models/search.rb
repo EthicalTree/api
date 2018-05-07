102ff121dcb626278de99c1ab9fdb4e0353f21c5
William Collins <wcurtiscollins@gmail.com>
2018-05-07 17:28:55 -0400

    fix duplicate location declaration

--- app/models/search.rb
+++ app/models/search.rb
@@ -11,10 +11,11 @@ class Search
 
       if directory_location.present?
         coords = directory_location.coordinates
-        results = results.by_distance(origin: coords)
 
         if filtered
           results = results.within(radius, units: :kms, origin: coords)
+        else
+          results = results.by_distance(origin: coords)
         end
       else
         location = MapApi.build_from_address(location)[:location]
