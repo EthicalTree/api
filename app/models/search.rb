dcb6203c5993ea8ecf46226cd1d5433f7e038297
William Collins <wcurtiscollins@gmail.com>
2018-05-07 19:49:56 -0400

    fix duplicate location in query

--- app/models/search.rb
+++ app/models/search.rb
@@ -11,30 +11,20 @@ class Search
 
       if directory_location.present?
         coords = directory_location.coordinates
-
-        if filtered
-          results = results.within(radius, units: :kms, origin: coords)
-        else
-          results = results.by_distance(origin: coords)
-        end
       else
         location = MapApi.build_from_address(location)[:location]
         coords = [location['lat'], location['lng']]
-        results = results.by_distance(origin: coords)
-
-        if filtered
-          results = results.within(radius, units: :kms, origin: coords)
-        end
       end
     else
       #coords = [location_information[:latitude], location_information[:longitude]]
       location = MapApi.build_from_address("Ottawa, ON")[:location]
       coords = [location['lat'], location['lng']]
-      results = results.by_distance(origin: coords)
+    end
 
-      if filtered
-        results = results.within(radius, units: :kms, origin: coords)
-      end
+    if filtered
+      results = results.within(radius, units: :kms, origin: coords)
+    else
+      results = results.by_distance(origin: coords)
     end
 
     results
