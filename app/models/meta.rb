c30b0f36c408b64d846c895f745094fcc1680cdd
William Collins <wcurtiscollins@gmail.com>
2018-05-26 00:14:29 -0400

    refactor how meta works

--- app/models/meta.rb
+++ app/models/meta.rb
@@ -13,50 +13,40 @@ class Meta
       return ""
     end
 
-    listing_slug = uri.path.scan(/\/listings\/[\w]+\/([\w-]+)\/?/)
-    curated_list_slug = uri.path.scan(/\/collections\/[\w]+\/([\w-]+)\/?/)
+    meta = {
+      name: 'EthicalTree',
+      description: 'Best of Ottawa restaurants, bakeries, cafés and stores that are Organic, Woman-owned, Fair Trade, Vegan and/or Vegetarian',
+      image: "#{Config.cdn}/images/et-social.png",
+      image_width: '1200',
+      image_height: '628'
+    }
+
+    listing_slug = uri.path.scan(/\/listings\/\w+\/([\w-]+)\/?/)
+    curated_list_slug = uri.path.scan(/\/collections\/\w+\/([\w-]+)\/?/)
 
     if listing_slug.present?
-      listing = Listing.find_by(slug: listing_slug)
-      get_meta_for_listing listing
+      if listing = Listing.find_by(slug: listing_slug[0][0])
+        meta.merge!({
+          name: listing.title,
+          description: listing.bio,
+          image: listing.cover_image,
+          image_width: '',
+          image_height: ''
+        })
+      end
     elsif curated_list_slug.present?
-      curated_list = CuratedList.find_by(slug: curated_list_slug)
-      get_meta_for_curated_list curated_list
-    else
-      generate_meta({
-        name: 'EthicalTree',
-        description: 'Best of Ottawa restaurants, bakeries, cafés and stores that are Organic, Woman-owned, Fair Trade, Vegan and/or Vegetarian',
-        image: "#{Config.cdn}/images/et-social.png",
-        image_width: '1200',
-        image_height: '628'
-      }) {|item| item}
+      if curated_list = CuratedList.find_by(slug: curated_list_slug[0][0])
+        meta.merge!({
+          name: curated_list.name,
+          description: curated_list.description,
+        })
+      end
     end
+
+    generate_meta(meta)
   end
 
   private
-
-  def self.get_meta_for_listing listing
-    if listing.present?
-      self.generate_meta({
-        name: listing.title,
-        description: listing.bio,
-        image: listing.cover_image
-      })
-    else
-      ''
-    end
-  end
-
-  def self.get_meta_for_curated_list curated_list
-    if curated_list.present?
-      self.generate_meta({
-        name: curated_list.name,
-        description: curated_list.description,
-      })
-    else
-      ''
-    end
-  end
 
   def self.generate_meta item
       item.each {|k, v| item[k] = CGI::escapeHTML(v || '')}
