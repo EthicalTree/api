8f807997847fe1e4370196e9d1db4c5f5814abfc
William Collins <wcurtiscollins@gmail.com>
2018-07-05 00:03:01 -0400

    get rid of unneccessary fields in sessions endpoint

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,6 @@ class SessionsController < APIController
 
   def index
     render json: {
-      x_forwarded_for: request.env['HTTP_X_FORWARDED_FOR'],
-      x_original_forwarded_for: request.env['HTTP_X_ORIGINAL_FORWARDED_FOR'],
       ip_address: remote_ip,
       location: location_information
     }, status: 200
