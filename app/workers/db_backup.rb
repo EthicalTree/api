d8f1219d97eaaf1272400749463ecceafb1a8898
William Collins <wcurtiscollins@gmail.com>
2018-07-19 16:36:00 -0400

    add db backup for wordpress

--- app/workers/db_backup.rb
+++ app/workers/db_backup.rb
@@ -1,6 +1,6 @@
 require 'sidekiq-scheduler'
 
-class DbBackup
+class ETDbBackup
   include Sidekiq::Worker
 
   def perform(*args)
@@ -10,16 +10,46 @@ class DbBackup
 
     app_root = File.join(File.dirname(__FILE__), "..", "..")
     settings = Rails.configuration.database_configuration[Rails.env]
+    database_name = settings['database']
 
     backup_filename = "#{settings['database']}-#{Time.now.strftime('%Y%m%d')}.sql"
     output_file = File.join(app_root, "tmp", backup_filename)
     password_flag = settings['password'].present? ? "-p'#{settings['password']}'" : ""
 
-    cmd = "/usr/bin/env mysqldump -h #{settings['host']} -u #{settings['username']} #{password_flag} #{settings['database']} > #{output_file}"
+    cmd = "/usr/bin/env mysqldump -h #{settings['host']} -u #{settings['username']} #{password_flag} #{database_name} > #{output_file}"
     system(cmd)
 
     $fog_db_backups.files.create({
-      key: backup_filename,
+      key: "ethicaltree/#{backup_filename}",
+      body: File.read(output_file),
+      public: false
+    })
+
+    File.delete(output_file) if File.exist?(output_file)
+  end
+end
+
+class BlogDbBackup
+  include Sidekiq::Worker
+
+  def perform(*args)
+    if Rails.env != 'production'
+      return
+    end
+
+    app_root = File.join(File.dirname(__FILE__), "..", "..")
+    settings = Rails.configuration.database_configuration[Rails.env]
+    database_name = 'ethicaltree_blog'
+
+    backup_filename = "blog-#{Time.now.strftime('%Y%m%d')}.sql"
+    output_file = File.join(app_root, "tmp", backup_filename)
+    password_flag = settings['password'].present? ? "-p'#{settings['password']}'" : ""
+
+    cmd = "/usr/bin/env mysqldump -h #{settings['host']} -u #{settings['username']} #{password_flag} #{database_name} > #{output_file}"
+    system(cmd)
+
+    $fog_db_backups.files.create({
+      key: "blog/#{backup_filename}",
       body: File.read(output_file),
       public: false
     })
