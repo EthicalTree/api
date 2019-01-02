f7023a7a522de027aaaf198f3d81a493f6b4ffaf
Calvin Collins <calvinc@surveymonkey.com>
2019-01-02 14:16:24 -0500

    show error message when daily quota is reached

--- app/models/map_api.rb
+++ app/models/map_api.rb
@@ -1,60 +1,63 @@
+# frozen_string_literal: true
+
 class MapApi
-  FromCoordinatesUrl = "https://maps.googleapis.com/maps/api/geocode/json"
-  FromAddressUrl = "https://maps.googleapis.com/maps/api/geocode/json"
-  TimezoneUrl = "https://maps.googleapis.com/maps/api/timezone/json"
+  FromCoordinatesUrl = 'https://maps.googleapis.com/maps/api/geocode/json'
+  FromAddressUrl = 'https://maps.googleapis.com/maps/api/geocode/json'
+  TimezoneUrl = 'https://maps.googleapis.com/maps/api/timezone/json'
 
-  def self.parse_results details
-    if !details.present?
-      return nil
+  def self.parse_results(details)
+    return nil unless details.present?
+
+    bounds = details['geometry']['viewport']
+    location = details['geometry']['location']
+
+    extracted = details['address_components'].each_with_object({}) do |c, a|
+      c['types'].each { |t| a[t.to_sym] = c }
     end
 
-    bounds = details["geometry"]["viewport"]
-    location = details["geometry"]["location"]
-
-    extracted = details["address_components"].reduce({}) do |a, c|
-      c["types"].each { |t| a[t.to_sym] = c }
-      a
-    end
-
-    latlng = { lat: location["lat"], lng: location["lng"] }
+    latlng = { lat: location['lat'], lng: location['lng'] }
 
     {
-      address: details["formatted_address"],
-      country: extracted[:country] ? extracted[:country]["short_name"] : '',
-      state: extracted[:administrative_area_level_1] ? extracted[:administrative_area_level_1]["short_name"] : '',
-      city: extracted[:locality] ? extracted[:locality]["short_name"] : '',
-      neighbourhood: extracted[:neighborhood] ? extracted[:neighborhood]["short_name"] : '',
-      sublocality: extracted[:sublocality] ? extracted[:sublocality]["short_name"] : '',
+      address: details['formatted_address'],
+      country: extracted[:country] ? extracted[:country]['short_name'] : '',
+      state: extracted[:administrative_area_level_1] ? extracted[:administrative_area_level_1]['short_name'] : '',
+      city: extracted[:locality] ? extracted[:locality]['short_name'] : '',
+      neighbourhood: extracted[:neighborhood] ? extracted[:neighborhood]['short_name'] : '',
+      sublocality: extracted[:sublocality] ? extracted[:sublocality]['short_name'] : '',
       bounds: {
-        northeast: bounds["northeast"],
-        southwest: bounds["southwest"]
+        northeast: bounds['northeast'],
+        southwest: bounds['southwest']
       },
       location: location,
       latlng: latlng,
-      timezone: self.get_timezone_details(location["lat"], location["lng"])
+      timezone: get_timezone_details(location['lat'], location['lng'])
     }
   end
 
-  def self.build_from_coordinates lat, lng
-    self.get_location_details "#{FromCoordinatesUrl}?latlng=#{lat},#{lng}"
+  def self.build_from_coordinates(lat, lng)
+    get_location_details "#{FromCoordinatesUrl}?latlng=#{lat},#{lng}"
   end
 
-  def self.build_from_address address
-    self.get_location_details "#{FromAddressUrl}?address=#{address}"
+  def self.build_from_address(address)
+    get_location_details "#{FromAddressUrl}?address=#{address}"
   end
 
-  def self.get_timezone_details lat, lng
+  def self.get_timezone_details(lat, lng)
     key = Rails.application.secrets.gmaps_api_key
     timestamp = Timezone.now.to_i
     url = "#{TimezoneUrl}?location=#{lat},#{lng}&timestamp=#{timestamp}&key=#{key}"
-    res = HTTParty.get(URI.parse(URI.encode("#{url}")))
-    JSON.parse(res.body)["timeZoneId"]
+    res = HTTParty.get(URI.parse(URI.encode(url.to_s)))
+    JSON.parse(res.body)['timeZoneId']
   end
 
-  def self.get_location_details url
+  def self.get_location_details(url)
     key = Rails.application.secrets.gmaps_api_key
     res = HTTParty.get(URI.parse(URI.encode("#{url}&key=#{key}")))
-    details = JSON.parse(res.body)["results"][0]
+    body = JSON.parse(res.body)
+
+    raise body['error_message'] if body['error_message']
+
+    details = body['results'][0]
     MapApi.parse_results details
   end
 end
